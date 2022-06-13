ea057a3dd2f96b760f99e553f0bf641c63e38ae7
Laurin Fäller <laurin.faeller@suse.com>
2022-06-13 14:50:32 +0200

    ci: remove dist folder from main branch

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/fuel-ignition/', // also needs to be changed in router/index.js
+  base: "/fuel-ignition/", // also needs to be changed in router/index.js
+  build: {
+    minify: false,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
