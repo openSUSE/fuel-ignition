1f576c57e8eef3175e0799524d347a77642d2e88
Ignaz Forster <iforster@suse.com>
2022-10-31 18:03:22 +0100

    Reimplement image writing using ArrayBuffer
    Converting the image contents to hex and doing manual endian conversions
    is unnecessary - just use DataView and ArrayBuffer instead.
    
    Fixes [gh#openSUSE/fuel-ignition#13]

--- src/components/TemplateBlobEditorComponent.vue
+++ src/components/TemplateBlobEditorComponent.vue
@@ -27,11 +27,15 @@ const blobEditor = new BlobEditor();
 
 const convertAndDownload = async function () {
   toggleLoading();
-  let image = await blobEditor.convertToImage(
-    props.ignJson,
-    props.combustionScript
-  );
-  blobEditor.downloadImageFile(image);
+  try {
+    let image = await blobEditor.convertToImage(
+      props.ignJson,
+      props.combustionScript
+    );
+    blobEditor.downloadImageFile(image);
+  } catch(err) {
+    alert(err);
+  }
   toggleLoading();
 };
 
@@ -79,39 +83,20 @@ export class BlobEditor {
     return result;
   }
 
+  // Properties of the generated file system image
   images = {
-    zero: { ignFatDirEntry: 0xf060 }, // 0xf07c
-    combustion: { ignFatDirEntry: 0xf0c0, combFatDirEntry: 0xf8c0 },
+    zero: { ignFatDirEntry: 0xf060, ignFileOffset: 0x15000 },
+    combustion: { ignFatDirEntry: 0xf0c0, combFatDirEntry: 0xf8c0, ignFileOffset: 0xf10800, combFileOffset: 0x318000 },
   };
 
   async fetchImage(imageName) {
     return fetch("templates/ignition-" + imageName + "-template.img").then(
-      (response) => response.blob()
+      (response) => response.arrayBuffer()
     );
-  }
-
-  applyPaddingAndLittleEndianIfNecessary(jsonLength, jsonLengthInHex) {
-    // pad length with a 0 if it's not even, so we can split more easily for big endian little endian conversion
-    if (jsonLengthInHex.length % 2 !== 0) {
-      jsonLengthInHex = "0" + jsonLengthInHex;
-    }
-
-    console.log("bigEndian :>>", jsonLengthInHex);
-
-    if (jsonLength > 255) {
-      // split every 2 characters into array, then reverse that array ("32 16 5F" => "5F 16 32")
-      jsonLengthInHex = jsonLengthInHex.match(/.{2}/g).reverse().join("");
-      console.log("littleEndian :>> ", jsonLengthInHex);
-    }
-
-    return jsonLengthInHex + new Array(9 - jsonLengthInHex.length).join("0"); // pad to 8 bytes, to override existing file size at offset
   }
 
   async convertToImage(json, combustionScript) {
     let jsonStr = JSON.stringify(json);
-    let jsonStrHex = jsonStr.hexEncode();
-    let jsonLength = jsonStr.length * 2; // in bytes
-    let jsonLengthInHex = jsonLength.toString(16);
     let imgTemplateName =
       combustionScript === undefined ? "zero" : "combustion"; // TODO: rename zero template to ignition-only
     let hasCombustion = combustionScript !== undefined;
@@ -121,100 +106,55 @@ export class BlobEditor {
       this.images[imgTemplateName]
     );
 
-    jsonLengthInHex = this.applyPaddingAndLittleEndianIfNecessary(
-      jsonLength,
-      jsonLengthInHex
-    );
-
-    let file = await this.fetchImage(imgTemplateName);
-
-    console.log(file);
-
-    console.log("jsonSize(dec): " + jsonStr.length * 2);
-    console.log("jsonSize(hex): " + jsonLengthInHex);
-
-    // https://de.wikipedia.org/wiki/File_Allocation_Table
+    let image = await this.fetchImage(imgTemplateName);
+    let imageView = new DataView(image);
 
     let dirEntry = this.images[imgTemplateName].ignFatDirEntry;
-    console.log("dirEntry :>> ", dirEntry);
-    let fileSizeOffset = (dirEntry + 0x1c) * 2; // 4 Bytes of size
+    let fileSizeView = new DataView(image, dirEntry + 0x1c, 4);
+    let encoder = new TextEncoder();
+    let jsonBin = encoder.encode(jsonStr);
+    let origIgnLength = fileSizeView.getInt32(0, true);
+    fileSizeView.setInt32(0, jsonBin.length, true);
 
+    if (jsonBin.length >= origIgnLength) {
+      throw new Error(
+        "Warning!\n\nIgnition configuration is too large.\n\n" +
+          "The maximum file size for the Ignition configuration is currently set to " + origIgnLength + "B."
+      );
+    }
+
+    for (let i = 0; i < jsonBin.length; i++) {
+      imageView.setInt8(this.images[imgTemplateName].ignFileOffset + i, jsonBin[i]);
+    }
+
+    console.log("jsonSize(string): " + jsonStr.length);
+    console.log("jsonSize(bin): " + jsonBin.length);
+    console.log("dirEntry :>> ", dirEntry);
     console.log(
       "images[" + imgTemplateName + "] :>> ",
       this.images[imgTemplateName]
     );
 
-    if (jsonLength > 15730099 || (hasCombustion && jsonLength > 7865049)) {
-      // number found through testing, although since the template config.ign file is about 15.3 MB it makes sense
-      alert(
-        "Warning!\n\nThe output img file system will most likely be corrupt, since the supplied JSON config is larger than 15.7 MB." +
-          "\n\nThis is a specific limitation of this config generator. Proceed at your own risk."
-      );
-    }
-
-    const buffer = await file.arrayBuffer().then();
-    let imgHexStr = this.bufferToHex(buffer);
-
-    console.log(
-      "fileSize slice:" + imgHexStr.slice(fileSizeOffset, fileSizeOffset + 8)
-    );
-
-    console.log("--- ignJsonHex ---");
-    console.log(jsonStrHex);
-    console.log("---");
-
-    imgHexStr = imgHexStr
-      .replace("6c6f72656d697073756d", jsonStrHex) // replace 'loremipsum' with our generated content
-      .replaceAt(fileSizeOffset, jsonLengthInHex); // set the properly formatted length at the FAT-Filesize offset
-
     if (hasCombustion && true) {
-      console.log("-- working on combustion hex --");
       let combDirEntry = this.images[imgTemplateName].combFatDirEntry;
-      let combFileSizeOffset = (combDirEntry + 0x1c) * 2; // 4 Bytes of size
+      fileSizeView = new DataView(image, combDirEntry + 0x1c, 4);
+      let combBin = encoder.encode(combustionScript);
+      let origCombLength = fileSizeView.getInt32(0, true);
+      fileSizeView.setInt32(0, combBin.length, true);
 
-      // let combScriptHex = this.strToHex(combustionScript);
-      let combScriptHex = combustionScript.hexEncode();
-      console.log("combustionScript :>> ", combustionScript);
-      console.log("--- cmbScriptHex ---");
-      console.log(combScriptHex);
-      console.log("---");
-      let combScriptLength = combustionScript.length * 2; // in bytes
-      let combScriptLengthInHex = combScriptLength.toString(16);
+      if (combBin.length >= origCombLength) {
+        throw new Error(
+          "Warning!\n\nCombustion configuration is too large.\n\n" +
+            "The maximum file size for the Combustion configuration is currently set to " + origCombLength + "B."
+        );
+      }
 
-      combScriptLengthInHex = this.applyPaddingAndLittleEndianIfNecessary(
-        combScriptLength,
-        combScriptLengthInHex
-      );
-
-      console.log("combScriptLengthInHex :>> ", combScriptLengthInHex);
-
-      imgHexStr = imgHexStr
-        .replace(
-          "646f6c6f72736974616d" +
-            new Array(combScriptLength * 2 - 19).join("0"),
-          combScriptHex
-        ) // replace 'dolorsitam' with combustion script content
-        .replaceAt(combFileSizeOffset, combScriptLengthInHex); // set the properly formatted length at the FAT-Filesize offset
+      for (let i = 0; i < combBin.length; i++) {
+        imageView.setInt8(this.images[imgTemplateName].combFileOffset + i, combBin[i]);
+      }
     }
 
-    console.log(
-      "fileSize slice:" + imgHexStr.slice(fileSizeOffset, fileSizeOffset + 8)
-    );
-
-    if (imgHexStr.length % 2) {
-      alert(
-        "Error: Cleaned hex string length is odd. Kindly open an issue on GitHub if the error persists."
-      );
-      return;
-    }
-
-    var binary = new Array();
-    for (var i = 0; i < imgHexStr.length / 2; i++) {
-      var h = imgHexStr.substr(i * 2, 2);
-      binary[i] = parseInt(h, 16);
-    }
-
-    var byteArray = new Uint8Array(binary);
+    var byteArray = new Uint8Array(image);
 
     return new Blob([byteArray], {
       type: "application/octet-stream",
