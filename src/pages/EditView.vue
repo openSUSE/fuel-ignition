e3144425ed797f0e90d92fec6e58bc7b28d1b30d
Stefan Schubert <schubi@suse.de>
2023-03-07 16:26:32 +0100

    Added Registration settings to combustion

--- src/pages/EditView.vue
+++ src/pages/EditView.vue
@@ -13,6 +13,7 @@ import ModifyServiceForm from "@/components/forms/ModifyServiceForm.vue";
 
 import DebugAddBytesForm from "@/components/forms/DebugAddBytesForm.vue";
 import DebugAnalyzeImgForm from "@/components/forms/DebugAnalyzeImgForm.vue";
+import CombRegistrationForm from "@/components/forms/combustion/CombRegistrationForm.vue";
 import CombInstallPackageForm from "@/components/forms/combustion/CombInstallPackageForm.vue";
 import CombAddRawBash from "../components/forms/combustion/CombAddRawBash.vue";
 
@@ -23,6 +24,7 @@ const formComponents = [
   AddNetworkForm,
   StartServiceForm,
   ModifyServiceForm,
+  CombRegistrationForm,
   CombInstallPackageForm,
   CombAddRawBash,
 
@@ -79,6 +81,8 @@ const toCombustionScript = (formData) => {
     console.log(json.combustion);
     json.output =
       "#!/bin/bash\n# combustion: network\n# script generated with https://opensuse.github.io/fuel-ignition/\n" +
+      "\n# Redirect output to the console\n" +
+      "exec > >(exec tee -a /dev/tty0) 2>&1\n" +
       json.combustion +
       '\n# Leave a marker\necho "Configured with combustion" > /etc/issue.d/combustion';
   }
@@ -185,6 +189,13 @@ const toCombustionScript = (formData) => {
           <div class="col-lg-6">
             <div class="form-floating mb-3">
               <FormKit type="group" v-model="formData">
+                <ExpandableComponent
+                  title="Product Registration"
+                  :displayAtLeastOne="false"
+                >
+                  <CombRegistrationForm></CombRegistrationForm>
+                </ExpandableComponent>
+
                 <ExpandableComponent
                   title="Install Package With Combustion"
                   :displayAtLeastOne="false"
