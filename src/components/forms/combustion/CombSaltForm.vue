3a891fe012b018ee2f1edafdacabe3abb14a305f
Stefan Schubert <schubi@suse.de>
2023-03-10 16:47:03 +0100

    added salt minion

--- src/components/forms/combustion/CombSaltForm.vue
+++ src/components/forms/combustion/CombSaltForm.vue
@@ -22,7 +22,9 @@
     <FormKit
       :name="formKey('minion_pub_key')"
       label="Minion pub key"
-      placeholder="ssh-rsa ......== user@company.com"
+      placeholder="-----BEGIN PUBLIC KEY-----
+...
+-----END PUBLIC KEY-----"
       type="textarea"
       validation-behavior="live"
       help="Public key for communitcation with the master."
@@ -31,6 +33,7 @@
       :name="formKey('enableservice')"
       label="Enable and start Salt Minion Service"
       type="checkbox"
+      :value=true
       validation-behavior="live"
       help="Whether the service will be enabled and started."
     />    
@@ -61,10 +64,29 @@ export default {
         .filter((x) => x.includes(keyPrefix))
         .map((key) => key.replace(keyPrefix, ""))
         .forEach((id) => {
-          json.combustion += "\n# Salt Menion\n" + 
+          json.combustion += "\n# Salt Menion\n" +
             "if ! which salt-minion > /dev/null 2>&1; then\n" +
             "    zypper --non-interactive install salt-minion\n" +
             "fi\n"
+	  if ( formValue("master", id) ) {
+	    json.combustion += "echo \"master: " +
+              formValue("master", id) + "\" \\\n >" +
+	      "/etc/salt/minion.d/fuel-ignition.conf\n"
+	  }
+          if ( formValue("minion_pem_key", id) ) {
+	    json.combustion += "echo \"" +
+              formValue("minion_pem_key", id) + "\" \\\n >" +
+	      "/etc/salt/pki/minion/minion.pem\n"
+	  }
+          if ( formValue("minion_pub_key", id) ) {
+	    json.combustion += "echo \"" +
+              formValue("minion_pub_key", id) + "\" \\ \n >" +
+	      "/etc/salt/pki/minion/minion.pub\n"
+	  }
+	  if ( formValue("enableservice", id) ) {
+            json.combustion += "systemctl enable salt-minion.service\n" +
+	      "systemctl start salt-minion.service\n"
+	  }
         });
     },
   },
