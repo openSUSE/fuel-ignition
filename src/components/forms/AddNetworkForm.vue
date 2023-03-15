c17fc63a1ea3acca0c85080844219c526ff99268
Stefan Schubert <schubi@suse.de>
2023-03-15 12:21:02 +0100

    removed all unneeded class definitions

--- src/components/forms/AddNetworkForm.vue
+++ src/components/forms/AddNetworkForm.vue
@@ -1,173 +1,170 @@
 <template>
-  <div class="addnetwork">
+  <FormKit
+    :name="formKey('interface')"
+    label="Interface Name"
+    type="text"
+    placeholder="e.g. eth0, wlan0,... "
+    validation="required"
+    validation-visibility="live"
+    help=" "
+  />
+
+  <FormKit
+    :name="formKey('ipv4_enabled')"
+    label="IPv4 Network enabled"
+    type="checkbox"
+    validation-behavior="live"
+    v-model="ipv4enabled"
+  />
+
+  <div v-if="ipv4enabled === true">
     <FormKit
-      :name="formKey('interface')"
-      label="Interface Name"
-      type="text"
-      placeholder="e.g. eth0, wlan0,... "
-      validation="required"
-      validation-visibility="live"
-      help=" "
+      type="select"
+      :name="formKey('ipv4_network_type')"
+      v-model="ipv4networkType"
+      label="IPv4 Network Address via"
+      help="Select how the network address of the interface will be set/evaluated."
+      :options="[
+        'DHCP',
+        'fixed IPv4 Address',
+      ]"
     />
+  </div>
 
+  <div v-if="ipv4networkType === 'fixed IPv4 Address' && ipv4enabled === true">
     <FormKit
-       :name="formKey('ipv4_enabled')"
-        label="IPv4 Network enabled"
-        type="checkbox"
-        validation-behavior="live"
-        v-model="ipv4enabled"
-      />
-
-    <div v-if="ipv4enabled === true" class="ipv4-network-type">
-      <FormKit
-        type="select"
-        :name="formKey('ipv4_network_type')"
-        v-model="ipv4networkType"
-        label="IPv4 Network Address via"
-        help="Select how the network address of the interface will be set/evaluated."
-        :options="[
-          'DHCP',
-          'fixed IPv4 Address',
-        ]"
-      />
-    </div>
-
-    <div v-if="ipv4networkType === 'fixed IPv4 Address' && ipv4enabled === true" class="fixed_ip_address">
-      <FormKit
-        :name="formKey('ipv4_address')"
-        label="IPv4 Address"
-        placeholder="___.___.___.___"
-        type="text"
-        :validation="[['required'],['matches', /^(([1-9]?\d|[12]\d\d)\.){3}([1-9]?\d|[12]\d\d)$/]]"
-        :validation-messages="{
-           matches: 'IP number must be in the format xxx.xxx.xxx.xxx',
-        }"
-	help="IPv4 address in the format xxx.xxx.xxx.xxx"
-        validation-visibility="live"
-      />
-      <FormKit
-        :name="formKey('ipv4_netmask')"
-        label="IPv4-Netmask"
-	type="number"
-	value="24"
-        validation="between:0,32"
-        validation-visibility="live"
-        help="Values between 0 and 32"
-      />
-      <FormKit
-        :name="formKey('ipv4_gateway')"
-        label="IPv4-Gateway"
-        placeholder="___.___.___.___"
-        type="text"
-        :validation="[['matches', /^(([1-9]?\d|[12]\d\d)\.){3}([1-9]?\d|[12]\d\d)$/]]"	
-        :validation-messages="{
-           matches: 'IP number must be in the format xxx.xxx.xxx.xxx',
-        }"
-	help="Gateway address in the format xxx.xxx.xxx.xxx"
-        validation-visibility="live"
-      />
-    </div>
-
+      :name="formKey('ipv4_address')"
+      label="IPv4 Address"
+      placeholder="___.___.___.___"
+      type="text"
+      :validation="[['required'],['matches', /^(([1-9]?\d|[12]\d\d)\.){3}([1-9]?\d|[12]\d\d)$/]]"
+      :validation-messages="{
+         matches: 'IP number must be in the format xxx.xxx.xxx.xxx',
+      }"
+      help="IPv4 address in the format xxx.xxx.xxx.xxx"
+      validation-visibility="live"
+    />
     <FormKit
-       :name="formKey('ipv6_enabled')"
-        label="IPv6 Network enabled"
-        type="checkbox"
-        validation-behavior="live"
-        v-model="ipv6enabled"
-      />
-
-    <div v-if="ipv6enabled === true" class="ipv6-network-type">
-      <FormKit
-        type="select"
-        :name="formKey('ipv6_network_type')"
-        v-model="ipv6networkType"
-        label="IPv6 Network Address via"
-        help="Select how the network address of the interface will be set/evaluated."
-        :options="[
-          'DHCP',
-          'fixed IPv6 Address',
-        ]"
-      />
-    </div>
-
-    <div v-if="ipv6networkType === 'fixed IPv6 Address' && ipv6enabled === true" class="fixed_ip_address">
-      <FormKit
-        :name="formKey('ipv6_address')"
-        label="IPv6 Address"
-        placeholder="____:____:____:____:____:____:____:____"
-        type="text"
-        :validation="[['required'],['matches', /^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7}$/]]"
-        :validation-messages="{
-           matches: 'IP number must be in the format ____:____:____:____:____:____:____:____',
-        }"
-	help="IPv6 address in the format ____:____:____:____:____:____:____:____"
-        validation-visibility="live"
-      />
-      <FormKit
-        :name="formKey('ipv6_netmask')"
-        label="IPv6-Netmask"
-	type="number"
-	value="64"
-        validation="between:0,128"
-        validation-visibility="live"
-        help="Values between 0 and 128"
-      />
-      <FormKit
-        :name="formKey('ipv6_gateway')"
-        label="IPv6-Gateway"
-        placeholder="____:____:____:____:____:____:____:____"
-        type="text"
-        :validation="[['matches', /^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7}$/]]"
-        :validation-messages="{
-           matches: 'IP number must be in the format ____:____:____:____:____:____:____:____',
-        }"
-	help="Gateway address in the format ____:____:____:____:____:____:____:____"
-        validation-visibility="live"
-      />
-    </div>
-
+      :name="formKey('ipv4_netmask')"
+      label="IPv4-Netmask"
+      type="number"
+      value="24"
+      validation="between:0,32"
+      validation-visibility="live"
+      help="Values between 0 and 32"
+    />
     <FormKit
-       :name="formKey('wifi_enabled')"
-        label="enable WiFi"
-        type="checkbox"
-        validation-behavior="live"
-        v-model="wifienabled"
-        help="Whether interface will be connected to WiFi."
-      />
+      :name="formKey('ipv4_gateway')"
+      label="IPv4-Gateway"
+      placeholder="___.___.___.___"
+      type="text"
+      :validation="[['matches', /^(([1-9]?\d|[12]\d\d)\.){3}([1-9]?\d|[12]\d\d)$/]]"	
+      :validation-messages="{
+         matches: 'IP number must be in the format xxx.xxx.xxx.xxx',
+      }"
+      help="Gateway address in the format xxx.xxx.xxx.xxx"
+      validation-visibility="live"
+    />
+  </div>
 
-    <div v-if="wifienabled === true" class="wifi">
+  <FormKit
+    :name="formKey('ipv6_enabled')"
+    label="IPv6 Network enabled"
+    type="checkbox"
+    validation-behavior="live"
+    v-model="ipv6enabled"
+  />
+
+  <div v-if="ipv6enabled === true">
+    <FormKit
+      type="select"
+      :name="formKey('ipv6_network_type')"
+      v-model="ipv6networkType"
+      label="IPv6 Network Address via"
+      help="Select how the network address of the interface will be set/evaluated."
+      :options="[
+        'DHCP',
+        'fixed IPv6 Address',
+      ]"
+    />
+  </div>
+
+  <div v-if="ipv6networkType === 'fixed IPv6 Address' && ipv6enabled === true">
+    <FormKit
+      :name="formKey('ipv6_address')"
+      label="IPv6 Address"
+      placeholder="____:____:____:____:____:____:____:____"
+      type="text"
+      :validation="[['required'],['matches', /^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7}$/]]"
+      :validation-messages="{
+         matches: 'IP number must be in the format ____:____:____:____:____:____:____:____',
+      }"
+      help="IPv6 address in the format ____:____:____:____:____:____:____:____"
+      validation-visibility="live"
+    />
+    <FormKit
+      :name="formKey('ipv6_netmask')"
+      label="IPv6-Netmask"
+      type="number"
+      value="64"
+      validation="between:0,128"
+      validation-visibility="live"
+      help="Values between 0 and 128"
+    />
+    <FormKit
+      :name="formKey('ipv6_gateway')"
+      label="IPv6-Gateway"
+      placeholder="____:____:____:____:____:____:____:____"
+      type="text"
+      :validation="[['matches', /^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7}$/]]"
+      :validation-messages="{
+         matches: 'IP number must be in the format ____:____:____:____:____:____:____:____',
+      }"
+      help="Gateway address in the format ____:____:____:____:____:____:____:____"
+      validation-visibility="live"
+    />
+  </div>
+
+  <FormKit
+    :name="formKey('wifi_enabled')"
+    label="enable WiFi"
+    type="checkbox"
+    validation-behavior="live"
+    v-model="wifienabled"
+    help="Whether interface will be connected to WiFi."
+  />
+
+  <div v-if="wifienabled === true">
+    <FormKit
+      :name="formKey('wifi_ssid_content')"
+      label="SSID"
+      type="text"
+      placeholder="Service Set IDentifier (SSID)"
+      validation="required"
+      validation-behavior="live"
+      help="Uniquely name of a wireless local area network (WLAN)."
+    />
+    <FormKit
+      type="select"
+      :name="formKey('key_mgmt')"
+      v-model="keymgmt"
+      label="Key Management"
+      help="Key management used for the connection."
+      :options="[
+        'none',
+        'WPA & WPA2 Personal',
+        'WPA3 Personal'
+      ]"
+      value='none'
+    />
+    <div v-if="keymgmt !== 'none'">
       <FormKit
-        :name="formKey('wifi_ssid_content')"
-        label="SSID"
+        :name="formKey('wifi_password_content')"
+        label="Password"
         type="text"
-	placeholder="Service Set IDentifier (SSID)"
-        validation="required"
-        validation-behavior="live"
-	help="Uniquely name of a wireless local area network (WLAN)."
+        help="Password required for the WiFi connection."
       />
-      <FormKit
-        type="select"
-        :name="formKey('key_mgmt')"
-        v-model="keymgmt"
-        label="Key Management"
-        help="Key management used for the connection."
-        :options="[
-          'none',
-          'WPA & WPA2 Personal',
-          'WPA3 Personal'
-        ]"
-	value='none'
-      />
-      <div v-if="keymgmt !== 'none'" class="wifi_keymgmt">
-        <FormKit
-          :name="formKey('wifi_password_content')"
-          label="Password"
-          type="text"
-          help="Password required for the WiFi connection."
-        />
-      </div>
     </div>
-
   </div>
 </template>
 
