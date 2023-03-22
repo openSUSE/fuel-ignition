b9df82fa4cbb5fea7d91d67a16dc2b6f8c18f07a
Stefan Schubert <schubi@suse.de>
2023-03-22 10:27:21 +0100

    fixed path

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -2,11 +2,11 @@ npm run build
 cp dist/index.html dist/404.html
 cp README.md dist/README.md
 git branch -D gh-pages
-git push public --delete gh-pages
+git push origin --delete gh-pages
 git branch gh-pages
 git checkout gh-pages
 git add dist/ -f
 git add dist/404.html
 git commit -m "feat(GitHub Pages): deploy latest change"
-git subtree push --prefix dist public gh-pages
+git subtree push --prefix dist origin gh-pages
 git checkout main
