f2d088c40a7db1a11e609884101350b380604252
Stefan Schubert <schubi@suse.de>
2023-08-09 10:00:18 +0200

    AddUsers testcase

--- cypress/e2e/AddHostname.cy.js
+++ cypress/e2e/AddHostname.cy.js
@@ -1,17 +1,16 @@
 describe('Setting hostname', () => {
   beforeEach(() => {
     cy.visit('/edit')
-    cy.contains('Ignition Config Generator')
   })
 
   it('creates ignition settings', () => {
     // load all fixtures
     cy.get('input[type=file]').selectFile('cypress/fixtures/AddHostname.json')
-
     cy.get('[data-testid=download_ignition]').click()
+
     // checking generated ignition file
     cy.readFile('cypress/downloads/config.ign').then((content) => { 
-      const parsed = JSON.parse(content);
+      const parsed = JSON.parse(content)
       cy.log(JSON.stringify(parsed.storage.files[0]))
       expect(parsed.storage.files[0]).to.have.all.keys('path', 'mode', 'overwrite', 'contents')
       expect(parsed.storage.files[0].path).to.equal('/etc/hostname')
