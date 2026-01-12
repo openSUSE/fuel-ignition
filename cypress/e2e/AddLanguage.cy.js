a51f944734eeb503b0817df607bfc2f302289481
Stefan Schubert <schubi@suse.de>
2026-01-12 18:08:35 +0100

    set Language via combustion

--- cypress/e2e/AddLanguage.cy.js
+++ cypress/e2e/AddLanguage.cy.js
@@ -6,17 +6,13 @@ describe('Setting language', () => {
   it('creates ignition settings', () => {
     // load fixtures
     cy.get('input[type=file]').selectFile('cypress/fixtures/AddLanguage.json')
-    cy.get('[data-testid=download_ignition]').click()
+    cy.get('[data-testid=download_combustion]').click()
 
-    // checking generated ignition file
-    cy.readFile('cypress/downloads/config.ign').then((content) => { 
-      const parsed = JSON.parse(content)
-      cy.log(JSON.stringify(parsed.storage.files[0]))
-      expect(parsed.storage.files[0]).to.have.all.keys('path', 'mode', 'overwrite', 'contents')
-      expect(parsed.storage.files[0].path).to.equal('/etc/locale.conf')
-      expect(parsed.storage.files[0].mode).to.equal(420)
-      expect(parsed.storage.files[0].overwrite).to.equal(true)
-      expect(parsed.storage.files[0].contents.human_read).to.equal('LANG=ast_ES@euro\n')
+    // checking generated combustion file
+    cy.readFile('cypress/downloads/script').then((content) => {
+      cy.log(content)
+      expect(content).to.have.string(
+        'echo \"LANG=ast_ES@euro\" > /etc/locale.conf')
     });
   })
 })
