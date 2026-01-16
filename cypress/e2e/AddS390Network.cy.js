05a5c9ed6fbb1bd043e5bfbeb405d9c9534e4022
Stefan Schubert <schubi@suse.de>
2026-01-16 11:29:27 +0100

    fixed testcase

--- cypress/e2e/AddS390Network.cy.js
+++ cypress/e2e/AddS390Network.cy.js
@@ -9,18 +9,6 @@ describe('Creating notwork devices', () => {
     cy.get('[data-testid=download_ignition]').click()    
     cy.get('[data-testid=download_combustion]').click()
     
-    // checking generated ignition file
-    cy.readFile('cypress/downloads/config.ign').then((content) => {
-      cy.log(content)
-      const parsed = JSON.parse(content)
-      
-      expect(parsed.storage.files[0].path).to.equal('/etc/NetworkManager/conf.d/noauto.conf')
-      expect(parsed.storage.files[0].mode).to.equal(420)
-      expect(parsed.storage.files[0].overwrite).to.be.true
-      expect(parsed.storage.files[0].contents.source).to.be.a('string')
-      expect(parsed.storage.files[0].contents.human_read).to.equal('[main]\n# Do not do automatic (DHCP/SLAAC) configuration on ethernet devices\n# with no other matching connections.\nno-auto-default=*\n')
-    });
-
     // checking generated combustion file
     cy.readFile('cypress/downloads/script').then((content) => {
       cy.log(content)
@@ -37,7 +25,11 @@ describe('Creating notwork devices', () => {
       expect(content).to.have.string(
         'cat >/etc/NetworkManager/system-connections/eth0.nmconnection <<-EOF')
       expect(content).to.have.string(
-        'address1=10.144.136.50/24,10.144.136.254')	
+        'address1=10.144.136.50/24,10.144.136.254')
+      expect(content).to.have.string(	
+	'cat >/etc/NetworkManager/conf.d/noauto.conf')
+      expect(content).to.have.string(
+	'[main]\n# Do not do automatic (DHCP/SLAAC) configuration on ethernet devices\n# with no other matching connections.\nno-auto-default=*\n')
     });
 
   })
